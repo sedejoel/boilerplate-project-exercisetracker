ac150a08997f2381315ff92cd1567c1f05c63996
sedejoel <sede.joel@gmail.com>
2023-09-17 03:43:55 -0400

    update

--- routes/exercice.js
+++ routes/exercice.js
@@ -3,31 +3,58 @@ const router = express.Router();
 const ExerciceModel = require('../models/exercice.model');
 const UserModel = require('../models/user.model');
 
+const formateDate = (param) => {
+    var moment = require('moment'); // require
+    let day = moment(param);
+    return day.toLocaleString().substring(0, 15);
+}
+
 router.post('/:_id/exercises', (req, res) => {
+
+    // console.log(req.body);
     const userId = req.params._id;
-    UserModel.find({ _id: userId }, (err, response) => {
+    UserModel.find({ _id: userId }, (err, user) => {
         if (err) {
             res.send({ error: 'ID utilisateur incorrect' });
         } else {
+            let date = '';
+            if ((typeof req.body.date === 'undefined')) {
+                let d = new Date().toISOString().substring(0, 10);
+                date = d;
+                // date = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDay();
+                //console.log(date);
+            } else {
+                date = req.body.date;
+            }
+
             let newExercice = new ExerciceModel({
-                id: userId,
+                id: user[0]._id,
                 description: req.body.description,
                 duration: req.body.duration,
-                date: (req.body.date === '') ? new Date() : req.body.date
+                date: date
             });
 
-            console.log(newExercice);
-            newExercice.save((err, newExercice) => {
+
+            newExercice.save((err, mExercice) => {
                 if (err)
                     res.send(err);
-                else
-                    res.send({
-                        _id: userId,
-                        username: response[0].username,
-                        description: newExercice.description,
-                        duration: newExercice.duration,
-                        date: new Date(newExercice.date).toDateString()
+                else {
+
+                    let r = {};
+
+                    r['description'] = mExercice.description;
+                    r.duration = mExercice.duration;
+                    r.date = formateDate(mExercice.date);
+                    r._id = mExercice.id;
+                    r.username = user[0].username;
+
+                    console.log(r);
+
+
+                    res.json({
+                        r
                     });
+                }
             })
 
         }
@@ -42,8 +69,6 @@ router.get('/:_id/logs', (req, res) => {
     const to = req.query.to;
     const limit = req.query.limit;
     let resultat = {};
-    console.log(from);
-
 
     UserModel.find({ _id: userId }, (err, response) => {
         if (err) {
@@ -63,7 +88,7 @@ router.get('/:_id/logs', (req, res) => {
                         else
                             resultat.count = resp.length;
                         const t = resp.map(el => {
-                            return { duration: el.duration, description: el.description, date: new Date(el.date).toDateString() };
+                            return { duration: el.duration, description: el.description, date: formateDate(el.date) };
                         });
                         resultat.log = t;
 
@@ -82,10 +107,10 @@ router.get('/:_id/logs', (req, res) => {
                             res.send(err)
                         else
                             resultat.count = resp.length;
-                        resultat.from = new Date(from).toDateString();
-                        resultat.to = new Date(to).toDateString();
+                        resultat.from = formateDate(from);
+                        resultat.to = formateDate(to);
                         const t = resp.map(el => {
-                            return { duration: el.duration, description: el.description, date: new Date(el.date).toDateString() };
+                            return { duration: el.duration, description: el.description, date: formateDate(el.date) };
                         });
                         resultat.log = t;
 
@@ -100,6 +125,8 @@ router.get('/:_id/logs', (req, res) => {
 
         }
     });
+
+
 
 });
 
---
+++ test.js
@@ -0,0 +1,8 @@
+var moment = require('moment'); // require
+let day = moment('2023-09-17');
+
+
+console.log(day.toLocaleString().substring(0, 15));
+
+//1990-01-01
+//1990-01-03
